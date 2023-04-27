d5b3c4652212406498aca0960b044016eaa6dea4
Amit Salaria <amit.salaria@payu.in>
2023-04-27 15:38:22 +0530

    Develop | CD | Swiftformat

--- SampleApp/swift-ui-ios/PayUCheckoutProWithSwiftUI/ContentView.swift
+++ SampleApp/swift-ui-ios/PayUCheckoutProWithSwiftUI/ContentView.swift
@@ -5,25 +5,26 @@
 //  Created by Amit Salaria on 20/02/23.
 //
 
-import SwiftUI
-import PayUCheckoutProKit
-import PayUCheckoutProBaseKit
-import PayUParamsKit
 import PayUBizCoreKit
+import PayUCheckoutProBaseKit
+import PayUCheckoutProKit
 import PayULoggerKit
+import PayUParamsKit
+import SwiftUI
 import UIKit
 
 let keySalt = [["3TnMpV", "g0nGFe03", Environment.production],
                ["gtKFFx", "4R38IvwiV57FwVpsgOvTXBdLE4tHUXFW", Environment.test]]
 let indexKeySalt = 1
+
 struct ContentView: View {
-    
+
     @State var labelWidth: CGFloat? = nil
-       @State var key = keySalt[indexKeySalt][0] as? String ?? ""
-       @State var salt = keySalt[indexKeySalt][1] as? String ?? ""
-       @State var environment = Utils.stringyfy(environment: keySalt[indexKeySalt][2])
-       @State var delegate:PayUCheckoutProDelegateClass!
-       @State private var isDisplayed = false
+    @State var key = keySalt[indexKeySalt][0] as? String ?? ""
+    @State var salt = keySalt[indexKeySalt][1] as? String ?? ""
+    @State var environment = Utils.stringyfy(environment: keySalt[indexKeySalt][2])
+    @State var delegate: PayUCheckoutProDelegateClass!
+    @State private var isDisplayed = false
     @State private var isNetBanking = false
     @State private var isCards = false
     @State private var isEMI = false
@@ -31,14 +32,10 @@ struct ContentView: View {
     @State private var isWallet = false
     @State var ccdc = ""
 
-
-
-
     var body: some View {
-        
-        
-        VStack{
-            HStack{
+
+        VStack {
+            HStack {
                 Text("Key")
                     .padding()
                 TextField("key", text: $key)
@@ -47,7 +44,7 @@ struct ContentView: View {
                     .background(Color.black.opacity(0.05))
                     .cornerRadius(10)
             }
-            HStack{
+            HStack {
                 Text("salt")
                     .padding()
                 TextField("salt", text: $salt)
@@ -56,7 +53,7 @@ struct ContentView: View {
                     .background(Color.black.opacity(0.05))
                     .cornerRadius(10)
             }
-            HStack{
+            HStack {
                 Text("env")
                     .padding()
                 TextField("environment", text: $environment)
@@ -64,18 +61,18 @@ struct ContentView: View {
                     .frame(width: 250, height: 50)
                     .background(Color.black.opacity(0.05))
                     .cornerRadius(10)
-                
+
             }
             Toggle("Enable Enforcement", isOn: $isDisplayed)
-                       .onChange(of: isDisplayed) { value in
-                           // action...
-                           print(value)
-                       }
-                       .padding()
-                       .frame(width: 300, height: 50)
-            
-            if isDisplayed == true{
-                
+                .onChange(of: isDisplayed) { value in
+                    // action...
+                    print(value)
+                }
+                .padding()
+                .frame(width: 300, height: 50)
+
+            if isDisplayed == true {
+
                 Toggle("Enforce NetBanking", isOn: $isNetBanking)
                     .onChange(of: isNetBanking) { value in
                         // action...
@@ -83,7 +80,7 @@ struct ContentView: View {
                     }
                     .padding()
                     .frame(width: 300, height: 50)
-                
+
                 Toggle("Enforce Cards", isOn: $isCards)
                     .onChange(of: isCards) { value in
                         // action...
@@ -91,13 +88,13 @@ struct ContentView: View {
                     }
                     .padding()
                     .frame(width: 300, height: 50)
-                
+
                 TextField("Credit/Debit", text: $ccdc)
                     .padding()
                     .frame(width: 250, height: 50)
                     .background(Color.black.opacity(0.05))
                     .cornerRadius(10)
-                
+
                 Toggle("Enforce EMI", isOn: $isEMI)
                     .onChange(of: isEMI) { value in
                         // action...
@@ -105,7 +102,7 @@ struct ContentView: View {
                     }
                     .padding()
                     .frame(width: 300, height: 50)
-                
+
                 Toggle("Enforce UPI", isOn: $isUPI)
                     .onChange(of: isUPI) { value in
                         // action...
@@ -113,7 +110,7 @@ struct ContentView: View {
                     }
                     .padding()
                     .frame(width: 300, height: 50)
-                
+
                 Toggle("Enforce Wallet", isOn: $isWallet)
                     .onChange(of: isWallet) { value in
                         // action...
@@ -122,7 +119,7 @@ struct ContentView: View {
                     .padding()
                     .frame(width: 300, height: 50)
             }
-            
+
             Button(action: {
                 openCheckoutPro()
             }) {
@@ -132,22 +129,21 @@ struct ContentView: View {
             .frame(width: 250, height: 50)
             .background(Color.blue)
             .cornerRadius(10)
-            
-            
+
         }
         .padding()
-       
+
     }
-    
+
     func openCheckoutPro() {
         let payUConfig = PayUCheckoutProConfig()
-        payUConfig.merchantName = "Umang Enterprises";
+        payUConfig.merchantName = "Umang Enterprises"
 //        payUConfig.paymentModesOrder = [self getPreferredPaymentMode];
-        if isDisplayed == true{
-            payUConfig.enforcePaymentList = getEnforcePaymentModesList() //"CARD", "UPI", "WALLET", "EMI",
+        if isDisplayed == true {
+            payUConfig.enforcePaymentList = getEnforcePaymentModesList() // "CARD", "UPI", "WALLET", "EMI",
         }
-        payUConfig.autoSelectOtp = true;
-        payUConfig.merchantResponseTimeout = 8;
+        payUConfig.autoSelectOtp = true
+        payUConfig.merchantResponseTimeout = 8
 
         let paymentParam = PayUPaymentParam(
             key: key,
@@ -159,83 +155,84 @@ struct ContentView: View {
             phone: "9876543210",
             surl: "https://payu.herokuapp.com/ios_success",
             furl: "https://payu.herokuapp.com/ios_failure",
-            environment: Utils.environment(environment: environment )
+            environment: Utils.environment(environment: environment)
         )
-        
+
         paymentParam.userCredential = "umang:arya"
         paymentParam.userToken = "test"
-        
+
         PayULog.shared.logLevel = .verbose
         delegate = PayUCheckoutProDelegateClass(salt: salt)
         PayUCheckoutPro.open(on: rootViewController!, paymentParam: paymentParam, config: payUConfig, delegate: delegate)
     }
+
     func getEnforcePaymentModesList() -> [[String: Any]]? {
         var enforcePaymentList = [[String: Any]]()
-        
+
         var nbEnforcement = [String: Any]()
         nbEnforcement[PaymentParamConstant.paymentType] = PaymentParamConstant.nb
-        
+
         var ccdcEnforcement = [String: Any]()
         ccdcEnforcement[PaymentParamConstant.paymentType] = PaymentParamConstant.card
-        
+
 //        if let cardType = ccdc, !cardType.isEmpty {
 //            let cardType = cardType.uppercased() == PaymentParamConstant.cc ? PaymentParamConstant.cc : PaymentParamConstant.dc
 //            ccdcEnforcement[PaymentParamConstant.cardType] = cardType
 //        }
-        
+
         var upiEnforcement = [String: Any]()
         upiEnforcement[PaymentParamConstant.paymentType] = PaymentParamConstant.upi
-        
+
         var walletEnforcement = [String: Any]()
         walletEnforcement[PaymentParamConstant.paymentType] = PaymentParamConstant.wallet
-        
+
         var emiEnforcement = [String: Any]()
         emiEnforcement[PaymentParamConstant.paymentType] = PaymentParamConstant.emi
-        
+
         var neftRtgsEnforcement = [String: Any]()
         neftRtgsEnforcement[PaymentParamConstant.paymentType] = PaymentParamConstant.neftrtgs
-        
+
         var sodexoEnforcement = [String: Any]()
         sodexoEnforcement[PaymentParamConstant.paymentType] = PaymentParamConstant.sodexo
-        
+
         var bnplEnforcement = [String: Any]()
         bnplEnforcement[PaymentParamConstant.paymentType] = PaymentParamConstant.bnpl
-        
-        
+
         if isNetBanking == true {
             enforcePaymentList.append(nbEnforcement)
         }
-        
+
         if isCards == true {
             enforcePaymentList.append(ccdcEnforcement)
         }
-        
+
         if isUPI == true {
             enforcePaymentList.append(upiEnforcement)
         }
-        
+
         if isWallet == true {
             enforcePaymentList.append(walletEnforcement)
         }
-        
+
         if isEMI == true {
             enforcePaymentList.append(emiEnforcement)
         }
-        
+
         return enforcePaymentList
     }
+
     var currentKeyWindow: UIWindow? {
-      UIApplication.shared.connectedScenes
-        .filter { $0.activationState == .foregroundActive }
-        .map { $0 as? UIWindowScene }
-        .compactMap { $0 }
-        .first?.windows
-        .filter { $0.isKeyWindow }
-        .first
+        UIApplication.shared.connectedScenes
+            .filter { $0.activationState == .foregroundActive }
+            .map { $0 as? UIWindowScene }
+            .compactMap { $0 }
+            .first?.windows
+            .filter { $0.isKeyWindow }
+            .first
     }
 
     var rootViewController: UIViewController? {
-      currentKeyWindow?.rootViewController
+        currentKeyWindow?.rootViewController
     }
 
 }
@@ -245,63 +242,63 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-    class PayUCheckoutProDelegateClass: PayUCheckoutProDelegate {
-        
-        let salt: String
-        
-        init(salt: String) {
-            self.salt = salt
-        }
-        
-        func onError(_ error: Error?) {
-            // handle error scenario
-            //            showAlert(title: "Error", message: error?.localizedDescription ?? "")
-            print("error\n",error ?? "")
-        }
-        
-        func onPaymentSuccess(response: Any?) {
-            // handle success scenario
-            //            showAlert(title: "Success", message: "\(response ?? "")")
-            print("response\n",response ?? "")
-        }
-        
-        func onPaymentFailure(response: Any?) {
-            // handle failure scenario
-            //            showAlert(title: "Failure", message: "\(response ?? "")")
-            print("response\n",response ?? "")
-        }
-        
-        func onPaymentCancel(isTxnInitiated: Bool) {
-            // handle txn cancelled scenario
-            // isTxnInitiated == YES, means user cancelled the txn when on reaching bankPage
-            // isTxnInitiated == NO, means user cancelled the txn before reaching the bankPage
-            let completeResponse = "isTxnInitiated = \(isTxnInitiated)"
-            //            showAlert(title: "Cancelled", message: "\(completeResponse)")
-            print(completeResponse)
-        }
-        
-        
-        func generateHash(for param: DictOfString, onCompletion: @escaping PayUHashGenerationCompletion) {
-            let commandName = (param[HashConstant.hashName] ?? "")
-            let hashStringWithoutSalt = (param[HashConstant.hashString] ?? "")
-            let postSalt = param[HashConstant.postSalt]
-            // get hash for "commandName" from server
-            // get hash for "hashStringWithoutSalt" from server
-            
-            // After fetching hash set its value in below variable "hashValue"
-            var hashValue = ""
-            if let hashType = param[HashConstant.hashType], hashType == HashConstant.V2 {
-                hashValue = PayUDontUseThisClass.hmacSHA256(hashStringWithoutSalt, withKey: salt)
-            } else if commandName == HashConstant.mcpLookup {
-                hashValue = Utils.hmacsha1(of: hashStringWithoutSalt, secret: "secret_key")
-            } else if let postSalt = postSalt {
-                let hashString = hashStringWithoutSalt + salt + postSalt
-                hashValue = Utils.sha512Hex(string: hashString)
-            } else {
-                hashValue = Utils.sha512Hex(string: (hashStringWithoutSalt + salt))
-            }
-            onCompletion([commandName : hashValue])
-        }
-        
+
+class PayUCheckoutProDelegateClass: PayUCheckoutProDelegate {
+
+    let salt: String
+
+    init(salt: String) {
+        self.salt = salt
     }
 
+    func onError(_ error: Error?) {
+        // handle error scenario
+        //            showAlert(title: "Error", message: error?.localizedDescription ?? "")
+        print("error\n", error ?? "")
+    }
+
+    func onPaymentSuccess(response: Any?) {
+        // handle success scenario
+        //            showAlert(title: "Success", message: "\(response ?? "")")
+        print("response\n", response ?? "")
+    }
+
+    func onPaymentFailure(response: Any?) {
+        // handle failure scenario
+        //            showAlert(title: "Failure", message: "\(response ?? "")")
+        print("response\n", response ?? "")
+    }
+
+    func onPaymentCancel(isTxnInitiated: Bool) {
+        // handle txn cancelled scenario
+        // isTxnInitiated == YES, means user cancelled the txn when on reaching bankPage
+        // isTxnInitiated == NO, means user cancelled the txn before reaching the bankPage
+        let completeResponse = "isTxnInitiated = \(isTxnInitiated)"
+        //            showAlert(title: "Cancelled", message: "\(completeResponse)")
+        print(completeResponse)
+    }
+
+    func generateHash(for param: DictOfString, onCompletion: @escaping PayUHashGenerationCompletion) {
+        let commandName = (param[HashConstant.hashName] ?? "")
+        let hashStringWithoutSalt = (param[HashConstant.hashString] ?? "")
+        let postSalt = param[HashConstant.postSalt]
+        // get hash for "commandName" from server
+        // get hash for "hashStringWithoutSalt" from server
+
+        // After fetching hash set its value in below variable "hashValue"
+        var hashValue = ""
+        if let hashType = param[HashConstant.hashType], hashType == "V2" {
+
+            hashValue = "<hmacSHA256 hash for hashStringWithoutSalt with salt>"
+        } else if commandName == HashConstant.mcpLookup {
+            hashValue = "<hmacsha1 hash for hashStringWithoutSalt and secret>"
+        } else if let postSalt = postSalt {
+            let hashString = hashStringWithoutSalt + (saltTextField.text ?? "") + postSalt
+            hashValue = "<hmacsha1 hash for hashStringWithoutSalt and secret>"
+        } else {
+            hashValue = "<hmacsha512 hash for hashStringWithoutSalt and salt>"
+        }
+        onCompletion([commandName: hashValue])
+    }
+
+}
--- SampleApp/swift-ui-ios/PayUCheckoutProWithSwiftUI/Utils.swift
+++ SampleApp/swift-ui-ios/PayUCheckoutProWithSwiftUI/Utils.swift
@@ -6,20 +6,21 @@
 //  Copyright © 2020 PayU Payments Pvt Ltd. All rights reserved.
 //
 
-import UIKit
 import CommonCrypto
-import PayUCheckoutProKit
-import PayUCheckoutProBaseKit
 import PayUBizCoreKit
+import PayUCheckoutProBaseKit
+import PayUCheckoutProKit
+import UIKit
 
 class Utils: NSObject {
-    
+
     // START: Remove from github app
     class func sha512Hex(string: String) -> String {
         PayUDontUseThisClass().getHash(string)
     }
+
     // END: Remove from github app
-    
+
     class func hmacsha1(of string: String, secret: String) -> String {
         PayUDontUseThisClass.hmacsha1(string, secret: secret)
     }
@@ -30,7 +31,7 @@ class Utils: NSObject {
         let txnID = "iOS" + formatter.string(from: Date())
         return txnID
     }
-    
+
     class func stringyfy(environment: Any?) -> String {
         guard let environment = environment as? Environment else {
             return ENVIRONMENT_PRODUCTION
@@ -42,24 +43,24 @@ class Utils: NSObject {
             return ENVIRONMENT_PRODUCTION
         }
     }
-    
+
     class func environment(environment: String) -> Environment {
         if environment == ENVIRONMENT_TEST {
-            return  Environment.test
+            return Environment.test
         } else {
-            return  Environment.production
+            return Environment.production
         }
     }
-    
+
     class func JSONFrom(string: String) -> Any? {
-        guard let data = string.data(using: .utf8) else { return nil}
+        guard let data = string.data(using: .utf8) else { return nil }
         do {
             return try JSONSerialization.jsonObject(with: data, options: [])
         } catch {
             return nil
         }
     }
-    
+
     class func paymentModeFrom(paymentType: String?, paymentOptionID: String?) -> PaymentMode? {
         var paymentMode: PaymentMode?
         if paymentType == "Cards".lowercased() {
@@ -75,38 +76,38 @@ class Utils: NSObject {
         }
         return paymentMode
     }
-    
+
     class func paymentTypeFrom(paymentType: String?) -> PaymentType? {
-        if (paymentType?.caseInsensitiveCompare("Cards") == .orderedSame) {
+        if paymentType?.caseInsensitiveCompare("Cards") == .orderedSame {
             return .ccdc
-        } else if (paymentType?.caseInsensitiveCompare("NetBanking") == .orderedSame) {
+        } else if paymentType?.caseInsensitiveCompare("NetBanking") == .orderedSame {
             return .netBanking
-        } else if (paymentType?.caseInsensitiveCompare("UPI") == .orderedSame) {
+        } else if paymentType?.caseInsensitiveCompare("UPI") == .orderedSame {
             return .upi
-        } else if (paymentType?.caseInsensitiveCompare("Wallet") == .orderedSame) {
+        } else if paymentType?.caseInsensitiveCompare("Wallet") == .orderedSame {
             return .wallet
-        } else if (paymentType?.caseInsensitiveCompare("emi") == .orderedSame) {
+        } else if paymentType?.caseInsensitiveCompare("emi") == .orderedSame {
             return .emi
-        } else if (paymentType?.caseInsensitiveCompare("SavedCard") == .orderedSame) {
+        } else if paymentType?.caseInsensitiveCompare("SavedCard") == .orderedSame {
             return .savedCard
-        } else if (paymentType?.caseInsensitiveCompare("NeftRtgs") == .orderedSame) {
+        } else if paymentType?.caseInsensitiveCompare("NeftRtgs") == .orderedSame {
             return .neftRtgs
-        } else if (paymentType?.caseInsensitiveCompare("Sodexo") == .orderedSame) {
+        } else if paymentType?.caseInsensitiveCompare("Sodexo") == .orderedSame {
             return .sodexo
         } else {
             return nil
         }
     }
-    
-    class func hexStringToUIColor(hex: String) -> UIColor? {
-        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
 
-        if (cString.hasPrefix("#")) {
+    class func hexStringToUIColor(hex: String) -> UIColor? {
+        var cString: String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+
+        if cString.hasPrefix("#") {
             cString.remove(at: cString.startIndex)
         }
         cString = String(cString.prefix(6))
 
-        var rgbValue:UInt64 = 0
+        var rgbValue: UInt64 = 0
         Scanner(string: cString).scanHexInt64(&rgbValue)
 
         return UIColor(
@@ -118,8 +119,8 @@ class Utils: NSObject {
     }
 }
 
-extension Date{
-    var dateString:String{
+extension Date {
+    var dateString: String {
         let formatter = DateFormatter()
         formatter.dateFormat = "dd/MM/yyyy"
         return formatter.string(from: self)
